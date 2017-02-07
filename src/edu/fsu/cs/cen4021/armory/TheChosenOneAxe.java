e5b364b7d71a467f99f9d4c094198dd584a0deb4
krc14b <krc14b@my.fsu.edu>
2017-02-07 11:14:16 -0500

    Updated Armory Classes Added TheChosenOneAxe Class and implemented it's unique features

---
+++ src/edu/fsu/cs/cen4021/armory/TheChosenOneAxe.java
@@ -0,0 +1,52 @@
+package edu.fsu.cs.cen4021.armory;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by kclem_000 on 2/6/2017.
+ */
+class TheChosenOneAxe extends BasicWeapon implements Weapon {
+
+    TheChosenOneAxe() {super(0);}
+
+    @Override
+    public int hit() {
+        int damage = DAMAGE;
+        try{
+            damage = dmg();
+        }catch (FileNotFoundException e){
+            System.out.println("Could not open file thechosenone.txt");
+        }
+        return damage;
+    }
+
+    @Override
+    public int hit(int armor){
+
+        int damage = DAMAGE;
+        try{
+            damage = dmg();
+        }catch (FileNotFoundException e){
+            System.out.println("Could not open file thechosenone.txt");
+        }
+        if(armor > 0 && armor < 20)
+            armor = 0;
+        damage = damage - armor;
+
+
+        return damage;
+    }
+
+    private int dmg() throws FileNotFoundException{
+        String a;
+        String b;
+        int counter = 1;        //Starts on Line 1
+        Scanner input = new Scanner(new File ("conf/thechosenone.txt"));
+        a = input.nextLine();
+        while(input.nextLine().equals(a)){
+            counter++;
+        }
+        return counter;
+    }
+}
